76b5d5e87e9ca5797e834371f338e408d98637ba
shafivullashaik-tech-ops <shafivullashaik916@gmail.com>
2026-03-08 22:03:32 +0530

    feat: create IAM module + wire EBS CSI driver IRSA role (fixes CrashLoopBackOff)

---
+++ terraform-aws-modules/iam/main.tf
@@ -0,0 +1,66 @@
+# IAM Module - IRSA Roles for EKS Service Accounts
+# Creates IAM roles for various EKS services using IRSA
+
+data "aws_partition" "current" {}
+
+################################################################################
+# EBS CSI Driver IRSA
+################################################################################
+
+resource "aws_iam_role" "ebs_csi_driver" {
+  count = var.create_ebs_csi_driver_role ? 1 : 0
+  name  = "${var.cluster_name}-ebs-csi-driver"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = var.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(var.oidc_provider_arn, "/^(.*provider/)/", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${replace(var.oidc_provider_arn, "/^(.*provider/)/", "")}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+
+  tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
+  count      = var.create_ebs_csi_driver_role ? 1 : 0
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_driver[0].name
+}
+
+################################################################################
+# ArgoCD IRSA (optional)
+################################################################################
+
+resource "aws_iam_role" "argocd" {
+  count = var.create_argocd_role ? 1 : 0
+  name  = "${var.cluster_name}-argocd"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = var.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(var.oidc_provider_arn, "/^(.*provider/)/", "")}:sub" = "system:serviceaccount:${var.argocd_namespace}:argocd-application-controller"
+          "${replace(var.oidc_provider_arn, "/^(.*provider/)/", "")}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+
+  tags = var.tags
+}
